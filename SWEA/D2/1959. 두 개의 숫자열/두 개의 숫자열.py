497e72802e5415e95069d7de7ad254b1f79eb265
Sanghyun <114604135+ddingorang@users.noreply.github.com>
2025-12-31 16:44:31 +0900

    [D2] Title: 두 개의 숫자열, Time: 60 ms, Memory: 51,200 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,33 +1,25 @@
-
 T = int(input())
 result = []
-
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    a, b = list(map(int, input('').split(' ')))
-    la = list(map(int, input('').split(' ')))
-    lb = list(map(int, input('').split(' ')))
+    n, m = tuple(map(int, input().split(' ')))
+    nl = list(map(int, input().split(' ')))
+    ml = list(map(int, input().split(' ')))
 
-    maxsum = 0
-    if a >= b:
-        for inc in range(a-b+1) :
-            tempsum = 0
-            for i in range(b) :
-                tempsum += la[i+inc] * lb[i]
-            if maxsum < tempsum :
-                maxsum = tempsum
+    maxsum = []
+    if n <= m:
+        for k in range(m-n+1) :
+            summ = 0
+            for i in range(n) :
+                summ += (nl[i] * ml[i+k])
+            maxsum.append(summ)
+    else :
+        for k in range(n-m+1) :
+            summ = 0
+            for i in range(m) :
+                summ += (nl[i+k] * ml[i])
+            maxsum.append(summ)
 
-    elif a < b :
-        for inc2 in range(b-a+1) :
-            tempsum2 = 0
-            for j in range(a) :
-                tempsum2 += la[j] * lb[j+inc2]
-            if maxsum < tempsum2 :
-                maxsum = tempsum2
+    result.append(max(maxsum))
 
-    result.append(maxsum)
-
-
-
-for index, r in enumerate(result):
-    print(f'#{index+1} {r}')
+for idx, e in enumerate(result):
+    print(f'#{idx+1} {e}')
