d1a424000bdcc7b4d72f671483f0f2b05835ffaf
Sanghyun <114604135+ddingorang@users.noreply.github.com>
2026-02-06 08:44:58 +0900

    [D3] Title: 햄버거 다이어트, Time: 785 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,77 +1,76 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-
+ 
     static int n;
     static int[] scores;
     static int[] calories;
-
+ 
     static boolean[] selected;
     static int maxcalories;
-
+ 
     static int resultmax;
-
+ 
     static void generateSubset(int cnt){
         if(cnt == scores.length) {
             int csum = 0;
             int ssum = 0;
             for(int i=0; i<n; i++) {
-                if(selected[i]) {
+                if(selected[i]) { // 고른 재료라면
                     csum += calories[i];
                     ssum += scores[i];
                 }
             }
-
-            if(csum <= maxcalories) {
-                if(ssum > resultmax) {
+ 
+            if(csum <= maxcalories) { // 최대 칼로리를 넘지 않는 선에서
+                if(ssum > resultmax) { // 최대 만족도가 갱신
                     resultmax = ssum;
-
+ 
                 }
             }
             return;
-
         }
-
+ 
         // 선택
         selected[cnt] = true;
         generateSubset(cnt+1);
-
+ 
         // 선택 안함
         selected[cnt] = false;
         generateSubset(cnt+1);
     }
-
+ 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int tc = Integer.parseInt(br.readLine());
         StringTokenizer st = null;
-
+ 
         for(int i=0; i<tc; i++) {
             st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
             maxcalories = Integer.parseInt(st.nextToken());
-
+ 
             scores = new int[n];
             calories = new int[n];
             selected = new boolean[n];
             resultmax = -1;
-
+ 
             for(int j=0; j<n; j++) {
                 st = new StringTokenizer(br.readLine());
-                scores[j] = Integer.parseInt(st.nextToken());
+                scores[j] = Integer.parseInt(st.nextToken()); // 만족도, 칼로리 각자 저장
                 calories[j] = Integer.parseInt(st.nextToken());
             }
-
+ 
             // 재료를 구하는 부분집합 싹 다 구하기
             // 재료가 최대 20개 : 부분집합 개수 최대 2의 20승 = 1,048,576개
             generateSubset(0);
-
+ 
             sb.append("#").append(i+1).append(" ").append(resultmax).append("\n");
         }
-
+ 
         System.out.println(sb);
     }
 }
