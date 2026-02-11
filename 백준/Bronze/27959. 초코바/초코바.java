e4ae848b08fa790a3e70ec4d73fe0f5fa8483d59
Sanghyun <114604135+ddingorang@users.noreply.github.com>
2026-02-12 08:32:53 +0900

    [Bronze V] Title: 초코바, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if(n*100 >= m) System.out.println("Yes");
+        else System.out.println("No");
+
+    }
+
+
+}
